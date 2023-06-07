5f55b258317d3081629cc9f6427063c0e2aa7b62
Taha Sarikaya <thsrky30@gmail.com>
2023-06-07 09:00:57 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button, TouchableOpacity, Linking } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState('Not yet scanned');
+
+  const askForCameraPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  };
+
+  // Request Camera Permission
+  useEffect(() => {
+    askForCameraPermission();
+  }, []);
+
+  // What happens when we scan the bar code
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data);
+    console.log('Type: ' + type + '\nData: ' + data);
+  };
+
+  // Handle link navigation
+  const handleLinkPress = async () => {
+    if (await Linking.canOpenURL(text)) {
+      await Linking.openURL(text);
+    }
+  };
+
+  // Check permissions and return the screens
+  if (hasPermission === null) {
+    return (
+      <View style={styles.container}>
+        <Text>Requesting for camera permission</Text>
+      </View>
+    );
+  }
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ margin: 10 }}>No access to camera</Text>
+        <Button title={'Allow Camera'} onPress={() => askForCameraPermission()} />
+      </View>
+    );
+  }
+
+  // Return the View
+  return (
+    <View style={styles.container}>
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={{ height: 400, width: 400 }}
+        />
+      </View>
+      <TouchableOpacity onPress={handleLinkPress}>
+        <Text style={styles.link}>{text}</Text>
+      </TouchableOpacity>
+      {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color="tomato" />}
+    </View>
+  );
+}
+
+// Styles 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  link: {
+    fontSize: 16,
+    margin: 20,
+    textDecorationLine: 'underline',
+    color: 'blue',
+  },
+  barcodebox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 300,
+    width: 300,
+    overflow: 'hidden',
+    borderRadius: 30,
+    backgroundColor: 'tomato',
+  },
+});
